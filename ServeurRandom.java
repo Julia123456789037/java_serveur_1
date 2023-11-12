ec5077a7d5a7cc5fc196fd9e10e88adb1bb6825d
Julia123456789037 <julia037.filia@gmail.com>
2023-11-12 21:55:16 +0100

    ajout des options
    ajout des option qui détermine les action du serve, ne renvoie que la date et quite le serv

--- ServeurRandom.java
+++ ServeurRandom.java
@@ -1,3 +1,5 @@
+package projet_java.Serveur.java_serveur_1;
+
 import java.net.*;
 import java.io.*;
 
@@ -23,6 +25,8 @@ public class ServeurRandom
 		Socket          toClient;
 		PrintWriter     out;
 		BufferedReader  in;
+		String			test;
+        boolean			sortir;
 		
 		/*-----------------*/
 		/* Instructions    */
@@ -37,18 +41,35 @@ public class ServeurRandom
 				ss = new ServerSocket(9000);
 				System.out.println ("client arrivé..."); 
 				toClient = ss.accept();
+				sortir = false;
+				DateFR d1;
 
 				out = new PrintWriter(toClient.getOutputStream(), true);
 
 				out.println("Bienvenue sur ServeurSimple1 de " + "Justine"); // bannière
 
-				DateFR d1;
-		
-
-				d1 = new DateFR ( );
-
-				out.println("Nous somme le " + d1); // bannière
-
+				in = new BufferedReader(new InputStreamReader(toClient.getInputStream()));
+                do 
+                {
+                    out.println( sortir );
+                    test = in.readLine();
+					d1 = new DateFR ( );
+                    if ( ! sortir || ! test.equals (""))
+                    {
+                        switch( test.charAt (0) )
+                        {
+                            case 'd' -> out.println("Nous somme le " + d1);
+                            case 'm' -> out.println("Nous somme le " + d1);
+                            case 'b' -> out.println("Bienvenue sur ServeurInfos de " + "Julia");
+                            case 'q' -> sortir = true;
+                            default -> out.println("defaut");
+                        }
+                    }
+                    else { sortir = true; }
+                    
+                    System.out.println (test);
+                    out.println(test);
+                }while ( sortir != true);
 
 
 				out.close();
